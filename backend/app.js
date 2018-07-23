2d3cba940815ed3217fe2bb7111b678d3ce8b7a0
Daniela Martin <danielamartinmtz@gmail.com>
2018-07-23 17:30:55 -0500

    Post in angular is working with the server

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    'Access-Control-Allow-Header',
+    'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader(
