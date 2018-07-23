94ada50a9b493387db3e7fca810be886cfd54681
Daniela Martin <danielamartinmtz@gmail.com>
2018-07-22 22:55:02 -0500

    Angular Material Added. textarea style added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
