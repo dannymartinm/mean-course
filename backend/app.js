e398233a7c94fca51bf1ded8288d14a99d5149be
Daniela Martin <danielamartinmtz@gmail.com>
2018-07-23 16:42:39 -0500

    Fetching initial posts through GET http://localhost:3000/api/posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '1234XX',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: '1234XYZ',
+      title: 'Second server-side post',
+      content: 'This is coming from the server!'
+    }
+  ];
+  return res.status(200).json({
+    message: 'Post fetch succesfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
