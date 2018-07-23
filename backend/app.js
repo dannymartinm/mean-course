6277a5cd0bd0f2079aae2c39148a36637688e74c
Daniela Martin <danielamartinmtz@gmail.com>
2018-07-23 17:19:41 -0500

    Post Endpointadded

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,6 +17,15 @@ app.use((req, res, next) => {
     'GET, POST, PATCH, DELETE, OPTIONS, PUT'
   );
   next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  //201: everything is ok. New resource is created.
+  res.status(201).json({
+    message: 'Post added sucessfully'
+  });
 });
 
 app.use('/api/posts', (req, res, next) => {
