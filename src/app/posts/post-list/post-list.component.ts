a177b0a441a7f0bf6da43d0184b991c756dc36c6
Daniela Martin <danielamartinmtz@gmail.com>
2018-07-23 00:06:13 -0500

    List of Posts added. Dummie values

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '../../../../node_modules/@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  // posts = [
+  //   {
+  //     title: 'First Post',
+  //     content: 'First posts content'
+  //   },
+  //   {
+  //     title: 'Second Post',
+  //     content: 'Second posts content'
+  //   },
+  //   {
+  //     title: 'Third Post',
+  //     content: 'Third posts content'
+  //   }
+  // ];
+  posts = [];
+}
